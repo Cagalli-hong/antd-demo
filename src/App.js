d2ff0f8109c1573e93fbadb5bb0baacc178f05d3
hongying2018 <hongying@ds.cn>
2018-11-22 16:55:36 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {button} from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="danger">Danger</Button>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +24,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
